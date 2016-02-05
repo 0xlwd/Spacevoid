0ac7e42889ae40458e081ddc8a36753f5dd5358e
AlexB <avtbonnet@hotmail.fr>
2016-02-05 17:04:33 +0100

    Added base for Post and User model

---
+++ model/PostModel.php
@@ -0,0 +1,18 @@
+<?php
+class PostModel{
+    public function CreatePost($title, $content, $user_related_id){
+
+    }
+
+    public function GetPost($id){
+
+    }
+
+    public function UpdatePost($title, $content, $user_related_id){
+
+    }
+
+    public function DeletePost($id){
+
+    }
+}
---
+++ model/UserModel.php
@@ -0,0 +1,18 @@
+<?php
+class UserModel{
+    public function CreateUser($login, $password, $mail, $description, $firstname, $lastname, $role){
+
+    }
+
+    public function GetUser($id){
+
+    }
+
+    public function UpdateUser($login, $password, $mail, $description, $firstname, $lastname, $role){
+
+    }
+
+    public function DeleteUser($id){
+
+    }
+}
