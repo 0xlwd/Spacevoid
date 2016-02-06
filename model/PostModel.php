6173b4a778009acc91ecf4112d4b001a97ed6147
AlexB <avtbonnet@hotmail.fr>
2016-02-06 20:37:50 +0100

    Update on Model Posts and Users

--- model/PostModel.php
+++ model/PostModel.php
@@ -1,18 +1,39 @@
 <?php
+require_once("DataBase/config.php");
 class PostModel{
     public function CreatePost($title, $content, $user_related_id){
-
+        global $pdo;
+        $database = $pdo->prepare("INSERT INTO `posts`(`title`, `content`, `user_related_id`)
+                          VALUES (:title,:content,:user_related_id)");
+        $database->bindParam("title",$title);
+        $database->bindParam("content",$content);
+        $database->bindParam("user_related_id",$user_related_id);
+        $database->execute();
     }
 
     public function GetPost($id){
-
+        global $pdo;
+        $database = $pdo->prepare("SELECT id FROM `posts`
+                          WHERE `id` = :id");
+        $database->bindParam("id",$id);
+        $database->execute();
     }
 
     public function UpdatePost($title, $content, $user_related_id){
-
+        global $pdo;
+        $database = $pdo->prepare("UPDATE `posts`
+                          SET `title`= :title,`content`= :content,`user_related_id`= :user_related_id");
+        $database->bindParam("title",$title);
+        $database->bindParam("content",$content);
+        $database->bindParam("user_related_id",$user_related_id);
+        $database->execute();
     }
 
     public function DeletePost($id){
-
+        global $pdo;
+        $database = $pdo->prepare("DELETE * FROM `posts`
+                          WHERE `id` = :id");
+        $database->bindParam("id",$id);
+        $database->execute();
     }
 }
--- model/UserModel.php
+++ model/UserModel.php
@@ -1,18 +1,49 @@
 <?php
+require_once("DataBase/config.php");
 class UserModel{
     public function CreateUser($login, $password, $mail, $description, $firstname, $lastname, $role){
-
+        global $pdo;
+        $database = $pdo->prepare("INSERT INTO `user`(`login`, `password`, `mail`,
+                          `description`, `firstname`, `lastname`, `role`)
+                          VALUES (:login,:password,:mail,:description,:firstname,
+                          :lastname,:role)");
+        $database->bindParam("login",$login);
+        $database->bindParam("password",$password);
+        $database->bindParam("mail",$mail);
+        $database->bindParam("description",$description);
+        $database->bindParam("firstname",$firstname);
+        $database->bindParam("lastname",$lastname);
+        $database->bindParam("role",$role);
+        $database->execute();
     }
 
     public function GetUser($id){
-
+        global $pdo;
+        $database = $pdo->prepare("SELECT id FROM user
+                          WHERE `id` = :id");
+        $database->bindParam("id",$id);
+        $database->execute();
     }
 
     public function UpdateUser($login, $password, $mail, $description, $firstname, $lastname, $role){
-
+        global $pdo;
+        $database = $pdo->prepare("UPDATE `user` SET `login`= :login,`password`= :password,`mail`= :mail,`description`= :description,
+                          `firstname`= :firstname,`lastname`= :lastname,`role`= :role");
+        $database->bindParam("login",$login);
+        $database->bindParam("password",$password);
+        $database->bindParam("mail",$mail);
+        $database->bindParam("description",$description);
+        $database->bindParam("firstname",$firstname);
+        $database->bindParam("lastname",$lastname);
+        $database->bindParam("role",$role);
+        $database->execute();
     }
 
     public function DeleteUser($id){
-
+        global $pdo;
+        $database = $pdo->prepare("DELETE * FROM user
+                          WHERE `id` = :id");
+        $database->bindParam("id",$id);
+        $database->execute();
     }
 }
